9bdf7b5b4413be2abd7da263a1d99b1119c9fd49
gianluca-peri <gianluca.peri@unifi.it>
2025-04-20 00:40:03 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,20 @@
+Command line interface for google gemini.
+
+This is a pet project of mine. Still an early version.
+
+Assuming a linux environment: to install ensure first that a recent version of `go` is installed on your system, and then run the following
+
+```bash
+go install github.com/gianluca-peri/cligem@latest
+```
+
+also be sure to add the following lines to your `.bashrc` file:
+
+```bash
+export YOUR_API_KEY
+export PATH=$PATH:/home/YOUR_USERNAME/go/bin
+```
+
+where of course you must substitute `YOUR_API_KEY` and `YOUR_USERNAME` with the correct values. Right now you can get a google API key for free (up to a limit).
+
+After the setup you can simply run a chat by typing `cligem` in your terminal.
---
+++ cligem.go
@@ -0,0 +1,72 @@
+package main
+
+// Useful link: https://ai.google.dev/gemini-api/docs/get-started/tutorial?lang=go
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/google/generative-ai-go/genai"
+	"google.golang.org/api/option"
+)
+
+func main() {
+	// Check if the environment variable is set
+	if os.Getenv("GEMINI_API_KEY") == "" {
+		fmt.Println("GEMINI_API_KEY environment variable is not set, please set it to your API key.")
+		return
+	}
+
+	// Get the API key from the environment variable
+	var gemini_api_key = os.Getenv("GEMINI_API_KEY")
+
+	// Choose the model to use
+	var model_name string = "gemini-1.5-flash"
+
+	ctx := context.Background()
+	client, err := genai.NewClient(ctx, option.WithAPIKey(gemini_api_key))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Close()
+
+	model := client.GenerativeModel(model_name)
+
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Println("Using model:", model_name)
+	fmt.Println("Please enter a string (or type 'quit' to quit):")
+	for {
+		// Read user input
+		user_input, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal("Failed to read input:", err)
+		}
+
+		// Remove the newline character from the input
+		user_input = user_input[:len(user_input)-1]
+
+		// Check if the user wants to quit
+		if user_input == "quit" {
+			break
+		}
+
+		// Generate content (text) using the model
+		resp, err := model.GenerateContent(ctx, genai.Text(user_input))
+		if err != nil {
+			log.Fatal("Failed to generate content (text):", err)
+		}
+
+		// Print the generated content
+		for _, candidate := range resp.Candidates {
+			for _, part := range candidate.Content.Parts {
+				if text, ok := part.(genai.Text); ok {
+					fmt.Println(text)
+				}
+			}
+		}
+	}
+}
